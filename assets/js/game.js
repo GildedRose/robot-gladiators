ec0f3cf6f6c711785bc7f5aaf356fe319da03c9e
Anne <anne.k.plummer@gmail.com>
2020-06-03 22:56:48 -0500

    created fight function for player and enemy robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,46 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log("This logs a string, good for leaving yourself a message");
-console.log(playerName);
-console.log(10+10);
-console.log("Our robor's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+console.log(playerName, playerAttack, playerHealth);
 
-// fight();
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // Alert users that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of 'playerAttack' from the value 'enemyHealth' and use that result to update the value in the 'enemyHeatlth' variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    //Log a resulting message to the console so we know that it worked
+    console.log(
+        playerName + " attacked " + enemyName + "." +  enemyName + " now has " + enemyHealth + " health remainging."
+    );
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    //Subtract the value of 'enemyAttack from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+
+    //Log a resulting message to the console so we know that it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+    //Check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+ };
+
+fight();
+
