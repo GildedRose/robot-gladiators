fb502c71936969ca3229f6b666c91779055bb968
Anne <anne.k.plummer@gmail.com>
2020-06-07 23:00:53 -0500

    game prompts for restart but only exits

--- assets/js/game.js
+++ assets/js/game.js
@@ -63,7 +63,7 @@ var fight = function(enemyName) {
     }    
 };       
 var startGame = function(){
-    for (car i = 0;) i < enemyNames.length; i++) {
+    for (var i = 0; i < enemyNames.length; i++) {
         if (playerHealth > 0){
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
@@ -73,8 +73,26 @@ var startGame = function(){
 
             fight(pickedEnemyName);
         }
-        else {
-            window.alert("You have lost your robot in battle! Game Over!");
-        }
     }
 };
+//function to end the entire game
+var endGame = function() {
+    if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+    window.alert("You've lost your robot in battle.");
+    }
+        //ask player if they're like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};      
+//start the game when the page loads
+startGame ();
+//after the loop ends, run endGame function
+endGame();
