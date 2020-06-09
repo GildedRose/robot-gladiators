5b1b48094a0bdeaeae732e1001e8aaa778478e90
Anne <anne.k.plummer@gmail.com>
2020-06-09 00:17:38 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,6 +13,13 @@ var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+//function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+};
+
 var fight = function(enemyName) {
     while(enemyHealth > 0 && playerHealth > 0) {
         //Ask user if they want to fight
@@ -29,9 +36,10 @@ var fight = function(enemyName) {
                 break;
             }
         }
-
         //remove enemy health
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -47,8 +55,9 @@ var fight = function(enemyName) {
              window.alert(enemyName + " still has " + enemyHealth + " health left.");
          }
 
-        //remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -72,7 +81,7 @@ var startGame = function(){
 
             var pickedEnemyName = enemyNames[i];
 
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
 
             fight(pickedEnemyName);
 
